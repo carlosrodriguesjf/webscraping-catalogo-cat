fe4ed3f2a2dd2e1a263568ab664a8d74317e76f5
Carlos Rodrigues <carlosrodriguesjf@gmail.com>
2023-06-20 22:51:57 -0300

    Implementado o scraping do site e e-mail

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Webscraping Catalogo telefonico
+# WEBSCRAPING CATÁLOGO TELEFÔNICO DA CAT
 
 # Importando as bibliotecas
 from bs4 import BeautifulSoup
@@ -15,24 +15,27 @@ conteudo = resposta.text
 soup = BeautifulSoup(conteudo, 'html.parser')
 print(10*'====='+'\n')
 
-# Teste com 5 grupos: len(setores)
-for i in range(0, 20):
-  # Encontrando os nomes dos setores
-  setores = soup.find_all('h3')
+# Looping para selecionar cada setor
+for i in range(0, 30):
 
-  # Exibindo o nome do setor
+  # Encontrando e exibindo os nomes dos setores
+  setores = soup.find_all('h3')
   print(setores[i].text+'\n')
 
   # Selecionando a coluna com o site, os telefones, os e-mails e redes sociais
   lista = soup.find_all('td', {'class':'column-2'})
-
   
-  # Selecionando o site
+  # Selecionando o site e o e-mail
   lista_site = lista[i].find_all('a',{'href':re.compile('http')})
-  if lista_site == []:
+  lista_emails = lista[i].find_all('a',{'href':re.compile('[\w\-.]+@[\w\-]+\.\w+')})
+
+  if lista_emails == [] or lista_site == []:
     site = ''
+    email = ''
   else:
     site = lista_site[0].text
+    email = lista_emails[0].text
     # Exibindo o site
   print(f'Site: {site}')
-  print(30*'-'+'\n')
+  print(f'E-mail: {email}')
+  print(30*'===='+'\n')
