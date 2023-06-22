b102aa4107a6809fea50907c38b026995c988ff6
Carlos Rodrigues <carlosrodriguesjf@gmail.com>
2023-06-21 22:13:57 -0300

    Implementado o scraping dos telefones

--- app.py
+++ app.py
@@ -15,8 +15,12 @@ conteudo = resposta.text
 soup = BeautifulSoup(conteudo, 'html.parser')
 print(10*'====='+'\n')
 
+contador = soup.find_all('h3')
+len(contador)
+
 # Looping para selecionar cada setor
-for i in range(0, 30):
+
+for i in range(0, len(contador)):
 
   # Encontrando e exibindo os nomes dos setores
   setores = soup.find_all('h3')
@@ -29,13 +33,27 @@ for i in range(0, 30):
   lista_site = lista[i].find_all('a',{'href':re.compile('http')})
   lista_emails = lista[i].find_all('a',{'href':re.compile('[\w\-.]+@[\w\-]+\.\w+')})
 
+  # Código para selecionar telefones 
+  telefones_code = str(lista[i])
+  soup_tel = BeautifulSoup(telefones_code,'html.parser')
+  soup_tel.get_text().find
+  lista_telefones = re.findall('\([1-9]{2}\) [0-9]{4}[-][0-9]{4}.*',telefones_code)
+
   if lista_emails == [] or lista_site == []:
     site = ''
     email = ''
   else:
     site = lista_site[0].text
     email = lista_emails[0].text
-    # Exibindo o site
+
+
+  # Exibindo o site
   print(f'Site: {site}')
   print(f'E-mail: {email}')
+  print('Telefone(s):')
+
+  for i in range(0,len(lista_telefones)):
+    telefone = lista_telefones[i].split('<')
+    print(f'{telefone[0]} ')
+
   print(30*'===='+'\n')
